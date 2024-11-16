38a3bd8fe0203caa68dfcbdd172e226f755f62bd
intercalaris <ymaviv@gmail.com>
2024-11-16 00:09:03 -0500

    finished

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://ymaviv:sisma@cluster0.hlyzd.mongodb.net/personal-express-app?retryWrites=true&w=majority', 
+    'dbName': 'photos'
+};
